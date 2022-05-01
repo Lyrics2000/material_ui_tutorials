3ecfafea7ec2a0f85ad30da44e297e47993ed135
lyrics <thomasambetsa@gmail.com>
2022-05-01 18:40:09 +0300

    aakki

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import React from 'react'
+import {withStyles} from '@material-ui/core/styles';
+import { Grid  ,Paper} from '@material-ui/core';
+
+
+const styles = theme => ({
+  root: {
+  flexGrow: 1
+  },
+  paper: {
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary
+  }
+  });
+
+function App() {
+  return (
+    <div className={styles.root}>
+
+    <Grid container spacing={1}>
+      <Grid item xs={12} sm={6} md={3}>
+         <Paper className={styles.paper}>xs=12 sm=6 md=3</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper className={styles.paper}>xs=12 sm=6 md=3</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper className={styles.paper}>xs=12 sm=6 md=3</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper className={styles.paper}>xs=12 sm=6 md=3</Paper>
+      </Grid>
+    </Grid>
+      
+    </div>
+  );
+}
+
+export default App;
