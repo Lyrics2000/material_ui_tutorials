10ebfb3efd7727665356b55042c41b1ce63386e4
lyrics <thomasambetsa@gmail.com>
2022-05-01 22:04:36 +0300

    added toolbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React from 'react'
 import GridComponents from './components/GridComponents';
 import Main from './components/abstract/Main';
 import HeaderAndFooter from './components/grid/HeaderAndFooter';
+import FixedPosition from './components/appbar/FixedPosition';
+import AppBarWithButtons from './components/appbar/AppBarWithButtons';
+import ToolbarAbstraction from './components/appbar/ToolbarAbstraction';
+
 
 function App() {
   return (
     <div >
-<HeaderAndFooter></HeaderAndFooter>
+<ToolbarAbstraction></ToolbarAbstraction>
     
       
     </div>
